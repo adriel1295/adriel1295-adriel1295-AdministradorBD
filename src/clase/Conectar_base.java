40c48f57723a02b7bdaa8369f5e5e9ee9d23dc34
Adriel Chavarría B <Adriel Chavarra B@DESKTOP-RDKBLV2>
2020-03-29 15:27:11 -0600

    Merge origin/Branch
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/Salidad_Llegadas/Controlador/Controlador_Bitacora.java
    	src/Salidad_Llegadas/Modelo/Bitacora.java
    	src/SalidasLLegadas/Bitacora.java
    	src/Vehiculo/Controlador/controladorVehiculo.java
    	src/Vehivulo/Modelo/Vehículo.java
    	src/clase/Conectar_base.java
    	src/clase/Main.java

--- src/clase/Conectar_base.java
+++ src/clase/Conectar_base.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -36,7 +36,6 @@ public class Conectar_base {
     
     
 }
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
